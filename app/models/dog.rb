32194a828838d49fdbe64ff2c2fd9d197ad71e93
Ayana Bando <ayanaflax@yahoo.com>
2018-09-27 15:20:26 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   attr_accessor :name, :breed, :age
   
-  # def initialization(name)
-  #   @name = name
-  #   @breed = breed
-  #   @age = age
-  # end
+  def initialization(name)
+    @name = name
+    @breed = breed
+    @age = age
+  end
 end
